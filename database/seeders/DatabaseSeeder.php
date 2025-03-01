f272ff2fc62fcc84659351a27deb1140a7dbce65
nurhabibrs <nurhabibrizkisaputro@gmail.com>
2025-03-01 13:39:49 +0700

    feat(User.php): implement FilamentUser interface in User model to support Filament package integration feat(DatabaseSeeder.php): update User model factory usage to create an unverified admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -12,11 +13,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        Models\User::factory()->unverified()->create([
+            'name' => 'Admin',
+            'email' => 'admin@mail.com',
+            'remember_token' => null,
+        ]);
     }
 }
